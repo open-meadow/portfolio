bf1e15327cb9bba7f89aa82b09fa4549f032dfa5
open-meadow <rohan.varughese@protonmail.com>
2023-02-23 17:20:46 -0500

    Further additions

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
     <>
       <Navigation />
       <main className="main-section">
-        <h2>Lorem Ipsum</h2>
+        <h1>Lorem Ipsum</h1>
         <div className="tools">
           <div className="tools--single">
             <img src="https://via.placeholder.com/30x30" />
@@ -29,6 +29,48 @@ const Projects = () => {
             <img src="https://via.placeholder.com/30x30" />
             <span>Tool</span>
           </div>
+        </div>
+
+        <hr className="line--first" />
+
+        <div className="working">
+
+          <div className="working--border">
+            <div className="working--details">
+              <h1 id="work">How it works</h1>
+              <hr className="line--second" />
+              <div id="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Elementum sagittis vitae et leo. Tristique nulla aliquet enim
+                tortor at. Aenean vel elit scelerisque mauris pellentesque. Orci
+                nulla pellentesque dignissim enim sit amet venenatis urna
+                cursus. Amet dictum sit amet justo donec enim. Mauris pharetra
+                et ultrices neque. Congue eu consequat ac felis donec et odio
+                pellentesque. Odio ut sem nulla pharetra diam.
+              </div>
+            </div>
+            <img src="https://via.placeholder.com/640x480" />
+          </div>
+
+          <div className="working--border">
+            <img src="https://via.placeholder.com/640x480" />
+            <div className="working--details--right">
+              <h1 id="notes">Programming Notes</h1>
+              <hr className="line--third" />
+              <div id="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Elementum sagittis vitae et leo. Tristique nulla aliquet enim
+                tortor at. Aenean vel elit scelerisque mauris pellentesque. Orci
+                nulla pellentesque dignissim enim sit amet venenatis urna
+                cursus. Amet dictum sit amet justo donec enim. Mauris pharetra
+                et ultrices neque. Congue eu consequat ac felis donec et odio
+                pellentesque. Odio ut sem nulla pharetra diam.
+              </div>
+            </div>
+          </div>
+
         </div>
       </main>
     </>
