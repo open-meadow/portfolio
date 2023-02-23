88f91a456cefc7b828a6074189602d12df90c332
open-meadow <rohan.varughese@protonmail.com>
2023-02-23 13:25:17 -0500

    Add placeholder project name and tools

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,36 @@
+import "../components/styles/Projects.scss";
+
 import Navigation from "../components/javascripts/Navigation";
 
 const Projects = () => {
   return (
     <>
       <Navigation />
+      <main className="main-section">
+        <h2>Lorem Ipsum</h2>
+        <div className="tools">
+          <div className="tools--single">
+            <img src="https://via.placeholder.com/30x30" />
+            <span>Tool</span>
+          </div>
+          <div className="tools--single">
+            <img src="https://via.placeholder.com/30x30" />
+            <span>Tool</span>
+          </div>
+          <div className="tools--single">
+            <img src="https://via.placeholder.com/30x30" />
+            <span>Tool</span>
+          </div>
+          <div className="tools--single">
+            <img src="https://via.placeholder.com/30x30" />
+            <span>Tool</span>
+          </div>
+          <div className="tools--single">
+            <img src="https://via.placeholder.com/30x30" />
+            <span>Tool</span>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
