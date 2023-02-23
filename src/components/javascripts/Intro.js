0eb5b8086bb071e232f1970e74bffb8b5aa6f00d
open-meadow <rohan.varughese@protonmail.com>
2023-02-22 19:25:41 -0500

    Add placeholder intro

---
+++ src/components/javascripts/Intro.js
@@ -0,0 +1,12 @@
+import "../styles/Intro.scss";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <p>Hi, I'm open-meadow</p>
+      <p>These are my projects.</p>
+    </div>
+  );
+};
+
+export default Intro;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Navigation from "../components/javascripts/Navigation";
+import Intro from "../components/javascripts/Intro";
 import Projects from "../components/javascripts/Projects";
 
 const Home = () => {
   return (
     <>
       <Navigation/>
+      <Intro/>
       <Projects/>
       <p>Poopoo</p>
     </>
