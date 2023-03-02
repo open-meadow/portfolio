8d7a0ce96b8bb88608fbd37d7c6c959273726fd5
open-meadow <rohan.varughese@protonmail.com>
2023-03-02 00:20:10 -0500

    Add credits and link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,19 @@ import Intro from "../components/javascripts/Intro";
 import ProjectSection from "../components/javascripts/ProjectSection";
 import About from "../components/javascripts/About";
 
+import "../components/styles/Home.scss";
+
 const Home = () => {
   return (
     <>
-      <Navigation/>
-      <Intro/>
-      <ProjectSection/>
-      <About/>
-      <hr/>
+      <Navigation />
+      <Intro />
+      <ProjectSection />
+      <About />
+      <hr />
+      <a href="https://www.pexels.com/photo/white-furred-animals-on-green-grass-field-710263/">
+        <p id="credit">Photo by Gabriel Peter from Pexels</p>
+      </a>
     </>
   );
 };
