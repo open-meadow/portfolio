aa9d78e06d92c000aa302001e327f4daa77ed7b2
open-meadow <rohan.varughese@protonmail.com>
2023-03-10 18:24:28 -0500

    Add info to About

--- src/components/javascripts/About.js
+++ src/components/javascripts/About.js
@@ -1,52 +1,70 @@
+import { useState } from "react";
 import "../styles/About.scss";
 
 const About = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
     <div id="About" className="about">
       <h1>About Me</h1>
       <h3>No, my name isn't actually open-meadow</h3>
       <div id="main">
-        <p id="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-          sagittis vitae et leo. Tristique nulla aliquet enim tortor at. Aenean
-          vel elit scelerisque mauris pellentesque. Orci nulla pellentesque
-          dignissim enim sit amet venenatis urna cursus. Amet dictum sit amet
-          justo donec enim. Mauris pharetra et ultrices neque. Congue eu
-          consequat ac felis donec et odio pellentesque. Odio ut sem nulla
-          pharetra diam.
-          <br />
-          <br />
-          Fames ac turpis egestas sed tempus urna. Facilisi morbi tempus iaculis
-          urna id. Erat pellentesque adipiscing commodo elit at imperdiet dui.
-          Vel pretium lectus quam id leo in. Justo laoreet sit amet cursus sit.
-          Netus et malesuada fames ac turpis. Nibh sit amet commodo nulla
-          facilisi nullam vehicula. Facilisi etiam dignissim diam quis enim
-          lobortis. Amet porttitor eget dolor morbi non arcu risus quis varius.
-          Aliquam purus sit amet luctus venenatis lectus magna fringilla urna.
-          Viverra tellus in hac habitasse platea dictumst vestibulum. Massa
-          ultricies mi quis hendrerit dolor. Sed enim ut sem viverra aliquet.
-          Metus dictum at tempor commodo ullamcorper. Ut morbi tincidunt augue
-          interdum velit euismod. 
-          <br />
-          <br />
-          Sit amet commodo nulla facilisi nullam vehicula. Ante in nibh
-          mauris cursus mattis molestie a iaculis at. Id aliquet risus feugiat
-          in. Sit amet venenatis urna cursus eget nunc scelerisque. Dolor sit
-          amet consectetur adipiscing elit duis tristique sollicitudin. Ipsum
-          dolor sit amet consectetur. Ut diam quam nulla porttitor massa id
-          neque aliquam. Velit scelerisque in dictum non consectetur a erat nam.
-          At auctor urna nunc id cursus metus aliquam. Quam id leo in vitae.
-          Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.
-          Suspendisse ultrices gravida dictum fusce ut placerat. Vivamus at
-          augue eget arcu dictum. Accumsan tortor posuere ac ut consequat semper
-          viverra nam. Dignissim sodales ut eu sem integer. Morbi blandit cursus
-          risus at ultrices. Tristique risus nec feugiat in fermentum posuere
-          urna. Elit ullamcorper dignissim cras tincidunt lobortis. Neque
-          gravida in fermentum et sollicitudin ac orci phasellus. Nibh venenatis
-          cras sed felis. Interdum consectetur libero id faucibus nisl
-          tincidunt.
-        </p>
+        <ul>
+          <li>
+            <h4 className="question">Then who am I?</h4>
+          </li>
+          <li>
+            <p className="answer">
+              My name is Rohan Varughese. (It's pronounced: Ro-hun V-uhr-geese)
+              Nice to meet you! This website is a way for me to show the
+              projects I've made so far. I hope you like them.
+            </p>
+          </li>
+          <li>
+            <h4 className="question">How did I make these?</h4>
+          </li>
+          <h5>
+            I typed {" "}
+            <span
+            id="alphabet"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              these symbols
+            </span>
+            {isHovering && (
+              <img
+                src={
+                  "https://www.publicdomainpictures.net/pictures/40000/velka/alphabet-letters-from-magazine.jpg"
+                }
+                id="alphabet--img"
+                alt="My Image"
+              />
+            )}
+            {" "} into Notepad in such a way that made these bottom things happy
+          </h5>
+          <ul>
+            <li>LANGUAGES: JavaScript, Ruby, HTML, CSS, Python, Java, C++</li>
+            <li>
+              FRAMEWORKS, LIBRARIES, AND ENVIRONMENTS: NodeJS, ReactJS, Ajax,
+              Express, EJS, jQuery, Bootstrap, ActiveRecord, React Native,
+              Rails, SASS, Linux{" "}
+            </li>
+            <li>TESTING: Storybook, Cypress, RSpec, Jest, Mocha, Chai </li>
+            <li>SYSTEMS AND DATABASES: SQL, Git</li>
+          </ul>
+          <li>
+            <h4 className="question">Where can I find myself?</h4>
+          </li>
+        </ul>
         <img src="https://via.placeholder.com/640x480" alt="placeholder" />
       </div>
     </div>
