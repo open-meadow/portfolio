66e5ea9e88143af162c9489e54176e57daa39d3d
open-meadow <rohan.varughese@protonmail.com>
2023-03-10 16:35:01 -0500

    Features and challenges for your-tube

--- src/db/projects/4_your-tube.js
+++ src/db/projects/4_your-tube.js
@@ -8,18 +8,7 @@ const yourTube = {
       Your-Tube is an alternative YouTube front-end that removes all external
       distractions to create a minimalist interface to help users focus on
       studies or work. It obtains data search and video data from the Invidious
-      API.It has the following features: Audio-only mode: Allows users to listen
-      to videos. (Helps to reduce bandwidth when listening to music
-      compilations) Ad-free streaming, Video Downloads, Toggle between Multiple
-      Players: Invidious player allows audio-only mode and ad-free streaming.
-      YouTube player allows faster loading and plays live-streams. Automatic
-      switching between instances: If an Invidious instance is down, the app
-      will automatically switch to a working one. Playlists: Players can create
-      custom playlists to add and delete videos. Videos accessed from playlists
-      have autoplay enabled as well as their own interface allowing easy access
-      to other videos. You will never need to alt-tab back to the browser.Zero
-      distractions: Clean interface with no unneeded tab elements. We aim to
-      keep you on our site as little as possible.
+      API.
     </div>
   ),
   features: (
@@ -28,28 +17,28 @@ const yourTube = {
         <h3>The website includes the following features:</h3>
         <ul id="small-margin">
           <li>
-            Upon inputting a long URL in the given form, the website generates a
-            short URL. This can be used to navigate to the long URL
+            Audio-only mode: Allows users to listen to videos. (Helps to reduce
+            bandwidth when listening to music compilations)
+          </li>
+          <li>Video Downloads: Download videos to watch later</li>
+          <li>
+            Toggle between Multiple Players: Invidious player allows audio-only
+            mode and ad-free streaming. YouTube player allows faster loading and
+            plays live-streams.
           </li>
           <li>
-            The Dashboard allows users to view a list of shortened URL's under
-            their account and quickly navigate to them, edit them or delete the
-            URL's.
+            Automatic switching between instances: If an Invidious instance is
+            down, the app will automatically switch to a working one.
           </li>
           <li>
-            An Edit screen allows users to quickly edit a URL under said ID
+            Playlists: Players can create custom playlists to add and delete
+            videos. Videos accessed from playlists have autoplay enabled as well
+            as their own interface allowing easy access to other videos. You
+            will never need to alt-tab back to the browser.
           </li>
           <li>
-            The screen keeps track of the number of times a URL is visited and
-            displays it to the user
-          </li>
-          <li>
-            Login and Registration capabilities allowing users to only view
-            their URL's
-          </li>
-          <li>
-            Login details are stored in cookies and are not lost upon browser
-            shutdown
+            Zero distractions: Clean interface with no unneeded tab elements. We
+            aim to keep you on our site as little as possible.
           </li>
         </ul>
       </div>
@@ -58,64 +47,89 @@ const yourTube = {
   challenges: [
     <div className="working--challenges--carousel--item">
       <div>
-        <h3>User Authentication</h3>
+        <h3>Audio-only mode</h3>
         <hr className="working--challenges--carousel--item--line" />
         <ul className="working--challenges--carousel--item--text">
           <li>
-            Upon filling the form and clicking "submit", the app checks if the
-            user exists in the database. As I hadn't yet learnt SQL, I was
-            storing the details in a JavaScript object.
+            This was also very tricky to implement. At one point, I was even
+            advised to give up on it and focus on something else.
           </li>
           <li>
-            If the user did not already exist, the app would create a new user.
-            It was then hash the given password using the bcrypt module from
-            npm.
+            I was trying to use the Invidious API to get the video. This was
+            difficult, because a lot of requests made would be blocked by the
+            browser's Cross Origin Resource Sharing policy.
           </li>
           <li>
-            During login, I had initially made an error. When logging in,
-            instead of checking if the supplied password matched the one in the
-            database, I had checked if the supplied password was equal to
-            itself. It was quite funny once it was pointed out to me, and I have
-            since fixed the mistake.
+            After viewing the open-source code from similar applications (as
+            well as having an e-mail chat with one of the developers of the
+            API), I was able to get the correct query string that would properly
+            send an audio and video back.{" "}
+          </li>
+          <li>
+            From there, I had to find how to use the audio-only player from the
+            video-js library I was using as a video player. It was a bit tricky,
+            but I was able to get it working.
           </li>
         </ul>
       </div>
       <img
         className="working--challenges--carousel--item--image"
-        src={images.tinyapp_loginPage}
+        src={images.yourTube_videoPageAudioMode}
         alt="project"
       />
     </div>,
     <div className="working--challenges--carousel--item">
       <div>
-        <h3>Analytics</h3>
+        <h3>Video Downloads</h3>
         <hr className="working--challenges--carousel--item--line" />
         <ul className="working--challenges--carousel--item--text">
           <li>
-            This was really tricky to implement. I actually spent a lot of time
-            trying to figure something out for this, and I had to get help from
-            a teacher.
+            This was tough. First, I had to make it so clicking the 'download'
+            button would use Axios to make a 'get' request to the server using
+            the provided video id.
           </li>
           <li>
-            I was trying to save the number of times a user views a page. For a
-            long time, I was using the cookie-session module from npm to store
-            the value in a cookie. This led to a long and complicated series of
-            steps where the page would crash if I hadn't passed the value
-            correctly to the page. Not to mention, there would be times when the
-            value would start incrementing from zero instead of one
+            On the server end, I had to install the ytdl and ffmpeg modules. I
+            used ytdl, I had to get the audio and video streams separately (as
+            it does not support combined hi-res downloads). From there, I used
+            ffmpeg to re-encode the two together and send it back to the client.
           </li>
           <li>
-            As a compromise, I ended up storing the value of timesViewed in the
-            database (a JS object), and I would directly call the value from the
-            database. It's a somewhat hacky solution. It wouldn't work at a
-            large scale, but for the purposes of the exercise, it got the job
-            done.
+            The video would then be automatically deleted from the server.
           </li>
         </ul>
       </div>
       <img
         className="working--challenges--carousel--item--image"
-        src={images.tinyapp_singleUrlPage}
+        src={images.yourTube_videoPageDownloads}
+        alt="project"
+      />
+    </div>,
+
+    <div className="working--challenges--carousel--item">
+      <div>
+        <h3>Switch between Invidious Instances</h3>
+        <hr className="working--challenges--carousel--item--line" />
+        <ul className="working--challenges--carousel--item--text">
+          <li>
+            Sometimes, a video would play on one Instance, but would not play on
+            another. I had initially chosen a specific instance, which could
+            reliably play most videos. However, I decided to try and safeguard
+            the app as much as possible.
+          </li>
+          <li>
+            I created a list of several available instances. I found this list
+            from the open-source code of the LibRedirect Firefox extension.
+          </li>
+          <li>
+            I then wrote a recursive function that would cycle through the list
+            to find the first working instance, and run said instance.
+          </li>
+        </ul>
+      </div>
+      <img
+        className="working--challenges--carousel--item--image"
+        src={images.yourTube_videoPageInvidiousVideo}
         alt="project"
       />
     </div>,
