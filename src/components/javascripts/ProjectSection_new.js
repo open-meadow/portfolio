1d2ec18deb63b868f11c833141224f3c4726c14b
open-meadow <rohan.varughese@protonmail.com>
2023-03-15 15:32:36 -0400

    Initial commit

---
+++ src/components/javascripts/ProjectSection_new.js
@@ -0,0 +1,11 @@
+import portfolio from "../../db/project_database";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProjectSection_new = () => {
+  return (<div>Hello World</div>);
+}
+
+export default ProjectSection_new;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,15 @@ import ProjectSection from "../components/javascripts/ProjectSection";
 import About from "../components/javascripts/About";
 
 import "../components/styles/Home.scss";
+import ProjectSection_new from "../components/javascripts/ProjectSection_new";
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Intro />
-      <ProjectSection />
+      {/* <ProjectSection /> */}
+      <ProjectSection_new />
       <About />
       <hr />
       <a href="https://www.pexels.com/photo/white-furred-animals-on-green-grass-field-710263/">
