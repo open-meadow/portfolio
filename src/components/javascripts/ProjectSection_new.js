0f3a00937e607d34fbccba6fd0361aedfbbe6f0e
open-meadow <rohan.varughese@protonmail.com>
2023-03-15 16:03:13 -0400

    Add basic slick carousel

--- src/components/javascripts/ProjectSection_new.js
+++ src/components/javascripts/ProjectSection_new.js
@@ -1,3 +1,5 @@
+import "../styles/ProjectSection_new.scss";
+
 import portfolio from "../../db/project_database";
 
 import Slider from "react-slick";
@@ -5,7 +7,37 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const ProjectSection_new = () => {
-  return (<div>Hello World</div>);
-}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+  };
+  return (
+    <div className="project">
+      <h2>Projects</h2>
+      <Slider {...settings} className="project--slider">
+        <div className="project--slider--item">
+          <h3>1</h3>
+        </div>
+        <div className="project--slider--item">
+          <h3>2</h3>
+        </div>
+        <div className="project--slider--item">
+          <h3>3</h3>
+        </div>
+        <div className="project--slider--item">
+          <h3>4</h3>
+        </div>
+        <div className="project--slider--item">
+          <h3>5</h3>
+        </div>
+        <div className="project--slider--item">
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default ProjectSection_new;
